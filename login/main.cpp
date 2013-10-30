24020b002a8778b9379df213f95995849677adb6
Catbug <catbug@hades.dev>
2013-10-30 16:16:27 +0100

    Configuration filename in arguments

--- login/main.cpp
+++ login/main.cpp
@@ -4,13 +4,17 @@
 #include "database/database.h"
 #include "utils/logger.h"
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "DNGS Zero - Login (DEV) by Catbug (" << __DATE__ << " " << __TIME__ << ")" << std::endl << std::endl;
 
     /** Initialisation of configuration file **/
     Config* config = Config::getInstance();
-    config->init("login.ini");
+
+    if(argc > 1)
+        config->init(argv[1]);
+    else
+        config->init("login.ini");
 
     /** Connect to PostgreSQL Database **/
     Database* database = Database::getInstance();
